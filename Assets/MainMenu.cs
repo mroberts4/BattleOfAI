546776f3b94eb9cb2cebf6ac82fee6a8524f91a7
Mariah Roberts <74730523+mariah-r@users.noreply.github.com>
2021-02-26 22:08:16 -0600

    Files for game
    Adding Unity Files for title screen

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ChangeToStoryScreen(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    
+    }
+
+    public void BackToMenu (int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+}
